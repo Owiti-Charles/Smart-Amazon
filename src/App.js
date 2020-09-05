c019bb34999b243f1124592cacd9f8ce8ab9c77f
Owiti-Charles <mikeycharlesm7@gmail.com>
2020-09-05 11:36:08 +0300

    Set up react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-    <h2>Amazon Boom</h2>
-
-    {/* react-Router */}
-    {/* home */}
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path='/checkout'>
+            <h2>Am in checkout</h2>
+          </Route>
+          <Route path="/login">
+            <h2>am in login </h2>
+          </Route>
+          <Route path = "/">
+            home
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
