5906b5bfc71624379306797486e7524a63bfa849
Owiti-Charles <mikeycharlesm7@gmail.com>
2020-09-06 12:14:05 +0300

    finish on barner

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import './Home.css';
+import React from "react";
+import "./Home.css";
 
 function Home() {
-    return (
-        <div className="home">
-            <img src="" alt="banner"/>
-        </div>
-    )
+  return (
+    <div className="home">
+      {/* https://ucarecdn.com/a55e2960-2f91-48f9-b418-4185725489c2/-/preview/1162x693/-/setfill/ffffff/-/format/jpeg/-/progressive/yes/ */}
+      {/* https://ucarecdn.com/e7c19fc3-c52f-4a8a-b8f2-0219ddb105df/-/preview/1162x693/-/setfill/ffffff/-/format/jpeg/-/progressive/yes/ */}
+      <img
+        className="home_image"
+        src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
+        alt="banner"
+      />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
